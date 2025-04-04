8ae9fbd1dacd714f442830db02231752b4d277cd
Emma-924 <151037998+Emma-924@users.noreply.github.com>
2025-04-04 10:38:16 +0200

    Update Profile.py

--- test/Profile.py
+++ test/Profile.py
@@ -1,4 +1,17 @@
 import streamlit as st
+import os
+
+st.set_page_config(page_title="Mon App", page_icon="favicon.png")
+    
+
+hide_streamlit_style = """
+    <style>
+    #MainMenu {visibility: hidden;}
+    footer {visibility: hidden;}
+    .st-emotion-cache-z5fcl4 {display: none;}  /* logo Streamlit */
+    </style>
+"""
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 st.title("Profile")
 sexe = st.radio("Sexe", ["Homme", "Femme", "Autre"])
@@ -9,5 +22,3 @@ poid = st.text_input("Poids")
 taille = st.text_input("Taille")
 type = st.radio("Type de diabète",["Type 1", "Type 2"])
 #st.write(f"Bonjour {x}")
-
-
